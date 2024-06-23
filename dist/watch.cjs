c1f0842a9ba070c74bd140c7cd34bb084531e320
Daniel Paixão <engsisdaniel.profissional@gmail.com>
2024-06-23 01:49:17 -0300

    FEATURE implementa uma escuta de outputs do terminal e dispara a mensagem para a api

---
+++ dist/watch.cjs
@@ -0,0 +1,52 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+
+// src/watch.js
+var import_axios = __toESM(require("axios"), 1);
+var path = require("path");
+var FormData = require("form-data");
+var { spawn } = require("child_process");
+var zapGptConnectUrl = "http://local.zapgptconnect.com.br";
+var ls = spawn(process.execPath, ["dist/index.cjs"], {
+  cwd: path.resolve(__dirname, ".."),
+  stdio: ["inherit", "pipe", "pipe"]
+}).on("error", (error) => {
+});
+ls.stdout.on("data", async (output) => {
+  const form = new FormData();
+  form.append("output", output);
+  const requestConfig = {
+    headers: {
+      ...form.getHeaders()
+    }
+  };
+  try {
+    const response = await import_axios.default.post(`${zapGptConnectUrl}/api/setlog/1234`, form, requestConfig);
+    console.log(`DATA:`);
+    console.log(response.data);
+  } catch (error) {
+    console.log(`ERROR: ${error.message}`);
+  }
+});
+ls.on("close", (code) => {
+});
---
+++ src/watch.js
@@ -0,0 +1,39 @@
+import Axios from 'axios';
+
+const path = require('node:path');
+const FormData = require('form-data');
+const { spawn } = require('node:child_process');
+
+require('dotenv').config()
+
+const zapGptConnectUrl = 'http://local.zapgptconnect.com.br'
+
+const ls = spawn(process.execPath, ['dist/index.cjs'], {
+    cwd: path.resolve(__dirname, '..'),
+    stdio: ["inherit", "pipe", "pipe"]
+}).on('error', (error) => {
+    // console.log(error);
+});
+
+ls.stdout.on('data', async (output) => {
+    const form = new FormData();
+    form.append('output', output);
+
+    const requestConfig = {
+        headers: {
+            ...form.getHeaders()
+        }
+    };
+
+    try {
+        const response = await Axios.post(`${zapGptConnectUrl}/api/setlog/${process.env.LIGHTSAIL_ID}`, form, requestConfig);
+        console.log(`DATA:`);
+        console.log(response.data);
+    } catch (error) {
+        console.log(`ERROR: ${error.message}`);
+    }
+});
+
+ls.on('close', (code) => {
+    // console.log(`child process exited with code ${code}`);
+}); 
